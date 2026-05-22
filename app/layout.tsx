1378e985b556d3350732e2a16f6ca16d5061adc3
maple <ae4222@gmail.com>
2026-05-22 13:15:13 +0700

    fix layout type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
+import React from "react";
 import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
